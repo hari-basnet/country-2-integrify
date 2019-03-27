c24095e33397355c6aa274444d423bd7a30fe2c7
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-03-27 11:42:31 +0200

    initial commit

---
+++ js /lesson.js
@@ -0,0 +1,14 @@
+const createContent = (content) => {
+   const {name, capital, language, population, flag} = content; 
+
+    
+    return `<div>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    
+    </div>`
+}
+
