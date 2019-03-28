facbbdd03d7f08d53da9139a048e6071c9b524d0
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-03-28 23:35:53 +0200

    new js file and video changed

--- js /lesson.js
+++ js /lesson.js
@@ -8,8 +8,8 @@ const countriesList = {
 
     },
 
-    createContent: function (content){
-        const {name, capital, languages, population, flag} = content;
+    createContent: (content) => {
+        const { name, capital, languages, population, flag } = content;
         return `<div class = "country-div">
             <img class= "flag-image" src="${flag}"/>
             <p>${name}</p>
@@ -20,9 +20,9 @@ const countriesList = {
 
     },
 
-    filterCountries: function (arr,search){
+    filterCountries: (arr, search) => {
         const filteredCountries = arr.filter(country => {
-            let {name, capital, languages} = country;
+            let { name, capital, languages } = country;
             let isName = name.toLowerCase().includes(search);
             let isCapital = capital.toLowerCase().includes(search);
             let isLanguages = languages.join().toLowerCase().includes(search);
@@ -33,30 +33,50 @@ const countriesList = {
         return result;
     },
 
-    showCountries: function (arr){
+    showCountries: (arr) => {
         let contents = '';
         resultSection.innerHTML = '';
-        arr.forEach((country, i) => {
+        arr.forEach((country) => {
             contents += countriesList.createContent(country);
         });
 
         resultSection.innerHTML = contents;
+    },
+
+    sortByName: (arr) => {
+        if (searchInput.value = '') {
+            showCountries(arr.sort().reverse());
+        } else {
+            showCountries(filterCountries(arr.sort().reverse(), searchInput.value.toLowerCase()));
+        }
+
+    },
+    sortByCapital: (arr) => {
+
     }
 
 };
 
-let {resultSection, subTitle, searchInput, buttons} = countriesList.elements;
-const { createContent, filterCountries, showCountries} = countriesList;
+let { resultSection, subTitle, searchInput, buttons } = countriesList.elements;
+const { createContent, filterCountries, showCountries, sortByName } = countriesList;
 
-buttons.addEventListener('click', e => {
-    console.log(e.target.className);
+
+subTitle.textContent = `Currently, we  have (${countriesObject.length}) countries`
+buttons.addEventListener('click', (e) => {
+    console.log(e.target.classList.contains('sort-by-name'));
+    if (e.target.classList.contains('sort-by-name')) {
+        sortByName(countriesObject);
+    }
 });
 
 
-searchInput.addEventListener('input', e =>{
+searchInput.addEventListener('keyup', (e) => {
     let searchTerm = e.target.value.toLowerCase();
     showCountries(filterCountries(countriesObject, searchTerm));
+    let count = document.querySelectorAll('.country-div');
+    subTitle.textContent = `Currently, we  have (${count.length}) countries`;
 });
 
-subTitle.textContent = `Currently, we  have ${countriesObject.length} countries`;
-showCountries(filterCountries(countriesObject, searchInput.value));
+//showCountries(filterCountries(countriesObject, searchInput.value));
+
+
--- js /script.js
+++ js /script.js
@@ -1,3 +1,26 @@
 // 
 
 //const resultSection = document.querySelector('.result-section');
+// const numbers = [1,2,4,5,6,7,5,3,23,56,7,53,13,3,1,2,];
+
+// let mySet = new Set(numbers);
+
+
+// console.log(mySet);
+
+// let myMap = new Map();
+
+// myMap.set('Helsinki', 'Finland');
+
+// console.log(myMap.get('Finland'));
+
+
+// let [one, two, three] = numbers;
+
+// console.log(one);
+
+// console.log(mySet);
+
+// for (let i = 0; i < 3; i ++){
+//     console.log(myMap);
+// }
