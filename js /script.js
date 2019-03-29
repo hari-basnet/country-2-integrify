b05ff1855fd85d4db585b9b6994356095f121342
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-03-29 09:26:27 +0200

    button actionlistener added

--- js /script.js
+++ js /script.js
@@ -1,26 +1,96 @@
-// 
+const resultSection = document.querySelector('.result-section');
+const subTitle = document.querySelector('.sub-title');
+const searchInput = document.querySelector('.search-input')
+const buttons = document.querySelector('.button-container');
 
-//const resultSection = document.querySelector('.result-section');
-// const numbers = [1,2,4,5,6,7,5,3,23,56,7,53,13,3,1,2,];
+const createContent=  (content) => {
+    const { name, capital, languages, population, flag } = content;
+    return `<div class = "country-div">
+        <img class= "flag-image" src="${flag}"/>
+        <p>${name}</p>
+        <p>${capital}</p>
+        <p>${languages.join(', ')}</p>
+        <p>${population.toLocaleString()}</p>
+    </div>`
 
-// let mySet = new Set(numbers);
+}
+
+const filterCountries =  (arr, search) => {
+    const filteredCountries = arr.filter(country => {
+        let { name, capital, languages } = country;
+        let isName = name.toLowerCase().includes(search);
+        let isCapital = capital.toLowerCase().includes(search);
+        let isLanguages = languages.join().toLowerCase().includes(search);
+
+        return isName || isCapital || isLanguages;
+    })
+    let result = search == '' ? arr : filteredCountries;
+    return result;
+}
+
+const showCountries =  (arr) => {
+    let contents = '';
+    resultSection.innerHTML = '';
+    arr.forEach((country) => {
+        contents += createContent(country);
+    });
+
+    resultSection.innerHTML = contents;
+}
+
+const sortByName =  (arr) => {
+    if (searchInput.value) {
+        showCountries(filterCountries(arr.sort().reverse(), searchInput.value.toLowerCase()));
+    } else {
+        showCountries(arr.sort().reverse());
+    }
+
+}
+
+const sortByCapital = (arr) => {
+    const sortedCapital = arr.sort(function(a, b) {
+        if (a.capital < b.capital) {
+          return 1;
+        }
+        if (a.capital > b.capital) {
+          return -1;
+        }
+        return 0;
+      });
+
+      if (searchInput.value) {
+          console.log('hello');
+        showCountries(filterCountries(sortedCapital, searchInput.value.toLowerCase()));
+    } else {
+        showCountries(sortedCapital.reverse());
+    }
+      
+}
+
+//console.log(sortByCapital(countriesObject));
 
 
-// console.log(mySet);
-
-// let myMap = new Map();
-
-// myMap.set('Helsinki', 'Finland');
-
-// console.log(myMap.get('Finland'));
 
 
-// let [one, two, three] = numbers;
 
-// console.log(one);
+subTitle.textContent = `Currently, we  have (${countriesObject.length}) countries`
+buttons.addEventListener('click', (e) => {
+    console.log(e.target.classList.contains('sort-by-name'));
+    console.log(e.target.classList.contains('sort-by-capital'));
 
-// console.log(mySet);
+    if (e.target.classList.contains('sort-by-name')) {
+        sortByName(countriesObject);
+    }else if (e.target.classList.contains('sort-by-capital')){
+        sortByCapital(countriesObject);
+    }else{
 
-// for (let i = 0; i < 3; i ++){
-//     console.log(myMap);
-// }
+    }
+});
+
+
+searchInput.addEventListener('keyup', (e) => {
+    let searchTerm = e.target.value.toLowerCase();
+    showCountries(filterCountries(countriesObject, searchTerm));
+    let count = document.querySelectorAll('.country-div');
+    subTitle.textContent = `Currently, we  have (${count.length}) countries`;
+});
