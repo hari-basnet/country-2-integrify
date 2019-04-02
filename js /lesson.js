c09885247d5d6c2fb73b9acf2855da4076458c51
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-04-02 16:59:05 +0300

    bug fixes

--- js /lesson.js
+++ js /lesson.js
@@ -1,82 +1,82 @@
-const countriesList = {
+// const countriesList = {
 
-    elements: {
-        resultSection: document.querySelector('.result-section'),
-        subTitle: document.querySelector('.sub-title'),
-        searchInput: document.querySelector('.search-input'),
-        buttons: document.querySelector('.button-container')
+//     elements: {
+//         resultSection: document.querySelector('.result-section'),
+//         subTitle: document.querySelector('.sub-title'),
+//         searchInput: document.querySelector('.search-input'),
+//         buttons: document.querySelector('.button-container')
 
-    },
+//     },
 
-    createContent: (content) => {
-        const { name, capital, languages, population, flag } = content;
-        return `<div class = "country-div">
-            <img class= "flag-image" src="${flag}"/>
-            <p>${name}</p>
-            <p>${capital}</p>
-            <p>${languages.join(', ')}</p>
-            <p>${population.toLocaleString()}</p>
-        </div>`
+//     createContent: (content) => {
+//         const { name, capital, languages, population, flag } = content;
+//         return `<div class = "country-div">
+//             <img class= "flag-image" src="${flag}"/>
+//             <p>${name}</p>
+//             <p>${capital}</p>
+//             <p>${languages.join(', ')}</p>
+//             <p>${population.toLocaleString()}</p>
+//         </div>`
 
-    },
+//     },
 
-    filterCountries: (arr, search) => {
-        const filteredCountries = arr.filter(country => {
-            let { name, capital, languages } = country;
-            let isName = name.toLowerCase().includes(search);
-            let isCapital = capital.toLowerCase().includes(search);
-            let isLanguages = languages.join().toLowerCase().includes(search);
+//     filterCountries: (arr, search) => {
+//         const filteredCountries = arr.filter(country => {
+//             let { name, capital, languages } = country;
+//             let isName = name.toLowerCase().includes(search);
+//             let isCapital = capital.toLowerCase().includes(search);
+//             let isLanguages = languages.join().toLowerCase().includes(search);
 
-            return isName || isCapital || isLanguages;
-        })
-        let result = search == '' ? arr : filteredCountries;
-        return result;
-    },
+//             return isName || isCapital || isLanguages;
+//         })
+//         let result = search == '' ? arr : filteredCountries;
+//         return result;
+//     },
 
-    showCountries: (arr) => {
-        let contents = '';
-        resultSection.innerHTML = '';
-        arr.forEach((country) => {
-            contents += countriesList.createContent(country);
-        });
+//     showCountries: (arr) => {
+//         let contents = '';
+//         resultSection.innerHTML = '';
+//         arr.forEach((country) => {
+//             contents += countriesList.createContent(country);
+//         });
 
-        resultSection.innerHTML = contents;
-    },
+//         resultSection.innerHTML = contents;
+//     },
 
-    sortByName: (arr) => {
-        if (searchInput.value = '') {
-            showCountries(arr.sort().reverse());
-        } else {
-            showCountries(filterCountries(arr.sort().reverse(), searchInput.value.toLowerCase()));
-        }
+//     sortByName: (arr) => {
+//         if (searchInput.value = '') {
+//             showCountries(arr.sort().reverse());
+//         } else {
+//             showCountries(filterCountries(arr.sort().reverse(), searchInput.value.toLowerCase()));
+//         }
 
-    },
-    sortByCapital: (arr) => {
+//     },
+//     sortByCapital: (arr) => {
 
-    }
+//     }
 
-};
+// };
 
-let { resultSection, subTitle, searchInput, buttons } = countriesList.elements;
-const { createContent, filterCountries, showCountries, sortByName } = countriesList;
-
-
-subTitle.textContent = `Currently, we  have (${countriesObject.length}) countries`
-buttons.addEventListener('click', (e) => {
-    console.log(e.target.classList.contains('sort-by-name'));
-    if (e.target.classList.contains('sort-by-name')) {
-        sortByName(countriesObject);
-    }
-});
+// let { resultSection, subTitle, searchInput, buttons } = countriesList.elements;
+// const { createContent, filterCountries, showCountries, sortByName } = countriesList;
 
 
-searchInput.addEventListener('keyup', (e) => {
-    let searchTerm = e.target.value.toLowerCase();
-    showCountries(filterCountries(countriesObject, searchTerm));
-    let count = document.querySelectorAll('.country-div');
-    subTitle.textContent = `Currently, we  have (${count.length}) countries`;
-});
+// subTitle.textContent = `Currently, we  have (${countriesObject.length}) countries`
+// buttons.addEventListener('click', (e) => {
+//     console.log(e.target.classList.contains('sort-by-name'));
+//     if (e.target.classList.contains('sort-by-name')) {
+//         sortByName(countriesObject);
+//     }
+// });
 
-//showCountries(filterCountries(countriesObject, searchInput.value));
+
+// searchInput.addEventListener('keyup', (e) => {
+//     let searchTerm = e.target.value.toLowerCase();
+//     showCountries(filterCountries(countriesObject, searchTerm));
+//     let count = document.querySelectorAll('.country-div');
+//     subTitle.textContent = `Currently, we  have (${count.length}) countries`;
+// });
+
+// //showCountries(filterCountries(countriesObject, searchInput.value));
 
 
